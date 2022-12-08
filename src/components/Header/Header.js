f9c47ff2116afa8c718708ef46ad98ac2f8467d4
Sandro Saldutto <sandro.saldutto@hotmail.ca>
2022-12-08 09:59:49 -0700

    added general content for my profile

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,29 +1,66 @@
 import "./Header.scss";
-import avatar from "../../assets/images/avatar.png";
+import sandro from "../../assets/images/sandro.png";
 import linkedinLogo from "../../assets/icons/linkedin.svg";
 import githubLogo from "../../assets/icons/github.svg";
-import headerTitle from "../../assets/icons/header-title.svg";
+import spotifyLogo from "../../assets/icons/spotify.svg";
+import appleLogo from "../../assets/icons/apple.svg";
+import sportChekLogo from "../../assets/icons/sport-chek.svg";
+import cobraLogo from "../../assets/icons/cobra.svg";
 
 function Header() {
   return (
     <section id="contact" className="header">
-      <img id="home" src={headerTitle} alt="sandro's avatar" className="header__title" />
-      <img src={avatar} alt="sandro's avatar" className="header__avatar" />
-      <div className="header__about-container">
+      <img src={sandro} alt="sandro" className="header__sandro" />
+      <div className="header__name-container">
+        <h2 className="header__name">Sandro Saldutto</h2>
+        <p className="header__role">Designer Developer</p>
+      </div>
+      <div className="header__bio-container">
+        <h3>Bio</h3>
+        <p className="header__bio">
+          Designer / Developer with the skill set, knowledge and understanding
+          of both the design and development stages.
+        </p>
+      </div>
+
+      <div className="header__contact-container">
+        <h3>Contact</h3>
         <div className="header__contact">
-          <a className="header__linkedin-link" href="https://www.linkedin.com/in/sandrosaldutto/" target="_blank" rel="noreferrer">
+          <a
+            className="header__linkedin-link"
+            href="https://www.linkedin.com/in/sandrosaldutto/"
+            target="_blank"
+            rel="noreferrer"
+          >
             <img
               src={linkedinLogo}
               alt="linkedin"
               className="header__linkedin"
             />
           </a>
-          <a className="header__github-link" href="https://github.com/sandrosaldutto" target="_blank" rel="noreferrer">
-          <img src={githubLogo} alt="github" className="header__github" />
+          <a
+            className="header__github-link"
+            href="https://github.com/sandrosaldutto"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <img src={githubLogo} alt="github" className="header__github" />
           </a>
         </div>
         <p className="header__email">sandro.saldutto@hotmail.ca</p>
         <p className="header__phone">416 271 3726</p>
+      </div>
+
+      <div className="header__brands-container">
+        <h3>Brands</h3>
+        <img src={spotifyLogo} alt="spotify" className="header__brand-logo" />
+        <img src={appleLogo} alt="apple" className="header__brand-logo" />
+        <img src={cobraLogo} alt="cobra" className="header__brand-logo" />
+        <img
+          src={sportChekLogo}
+          alt="sport chek"
+          className="header__brand-logo"
+        />
       </div>
     </section>
   );
